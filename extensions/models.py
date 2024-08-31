67c5111307525382bf3d764c4cba3c0166d0ee46
creaink <455372644@qq.com>
2024-08-31 18:07:08 +0800

    add get_mysql_table_explain_rows for model approximate count

--- extensions/models.py
+++ extensions/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 from django.db import models
+from django.db.models.sql.query import Query
 from django.core.paginator import Paginator
 from django.utils.functional import cached_property
 
@@ -22,8 +23,25 @@ class TableHelper():
             logger.exception(e)
             return 1
 
+    @classmethod
+    def get_mysql_table_explain_rows(cls, query: Query) -> int:
+        from django.db import connection
+        try:
+            with connection.cursor() as cursor:
+                # str query will get wrong params format sql
+                # ref https://code.djangoproject.com/ticket/17741
+                sql, params = query.sql_with_params()
+                cursor.execute(f"EXPLAIN {sql}", params)
+                row = cursor.fetchone()
+                # explain rows col as approximate rows
+                return row[9]
+        except Exception as e:
+            logger.exception(e)
+            return 1
+
 class ApxTotalCountAdminPaginator(Paginator):
     @cached_property
     def count(self):
         # TODO multi db type support
-        return TableHelper.get_mysql_table_approximate_rows(self.object_list.model)
+        # return TableHelper.get_mysql_table_approximate_rows(self.object_list.model)
+        return TableHelper.get_mysql_table_explain_rows(self.object_list.query)
